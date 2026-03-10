6a2499c707738597ed88d09bb2319a1171e983d3
eduardo-hdez <ehdez2004@gmail.com>
2026-03-09 18:25:29 -0600

    [Feat] build add_concat function

---
+++ regex_to_dfa/helper.py
@@ -0,0 +1,11 @@
+def add_concat(regex):
+    result = []
+    for i, c in enumerate(regex):
+        result.append(c)
+        if i + 1 < len(regex):
+            left, right = c, regex[i + 1]
+            left_ok  = left  not in "(|"
+            right_ok = right not in ")|*+?"
+            if left_ok and right_ok:
+                result.append('.')
+    return ''.join(result)
